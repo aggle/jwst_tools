00cc059f07a4d9acd43cf4b94293fe4b4d5312a0
Jonathan Aguilar <jaguilar@stsci.edu>
2022-09-19 13:24:50 -0400

    added some helper functions

--- jwst_tools/plot_utils.py
+++ jwst_tools/plot_utils.py
@@ -250,3 +250,32 @@ def visual_ta_check(filename, siaf_aper, pad=10):
         siaf_aper.plot(frame='det', ax=ax, mark_ref=True, fill=False)
     fig.suptitle(siaf_aper.AperName)
     return fig
+
+def set_up_multifig(nimgs, ncols, scalesize, kwargs={}):
+    """
+    Wrapper for the usual things I do to set up a multiple axes figure
+
+    Parameters
+    ----------
+    nimgs: int
+      the number of images
+    ncols: int
+      the number of columns (the number of rows will be computed)
+    scalesize: float
+      use this to scale the figure size; will be proportional to fig dimensions
+    kwargs: dict (default: {})
+      any other arguments to pass to plt.subplots()
+
+    Output
+    ------
+    tuple of (fig, axes)
+
+    """
+    nrows = np.ceil(nimgs/ncols).astype(int)
+    fig, axes = plt.subplots(nrows=nrows, ncols=ncols,
+                             figsize=(scalesize*ncols, scalesize*nrows),
+                             squeeze=False,
+                             **kwargs)
+    for ax in axes.ravel()[nimgs:]:
+        ax.set_visible(False)
+    return (fig, axes)
