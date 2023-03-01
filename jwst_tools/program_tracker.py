d10ee1cc09120264c91e0e1c9c142ea3634d892e
Jonathan Aguilar <jaguilar@stsci.edu>
2023-03-01 16:34:39 -0500

    working website
    working html page with separate sortable, searchable tables for different
    scheduling statuses

--- jwst_tools/program_tracker.py
+++ jwst_tools/program_tracker.py
@@ -1,12 +1,13 @@
 """Generate an html page that contains all the visit information"""
 
+import sys
 import re
 from pathlib import Path
 from datetime import date
 
 from requests.sessions import get_environ_proxies
 import pandas as pd
-from . import observing_windows as ow
+import observing_windows as ow
 
 template_path = Path(__file__).parent / "html_templates"
 
@@ -49,15 +50,21 @@ def add_plan_windows_to_program_df(program_df):
     """
     Given the results of the web crawl for program info, parse the program windows
     into their own columns for beginning and end dates
+    Applies the parse_plan_windows() function to the scraper results
     """
+    # programs that have already executed do not have a planWindow column
+    # if this is the case, add a dummy column
+    if "planWindow" not in program_df.columns:
+        program_df["planWindow"] = pd.NA
+
     parsed = {i: parse_plan_windows(row['planWindow'], [])
-              for i, row in programs_df.iterrows()}
+              for i, row in program_df.iterrows()}
     parsed = pd.concat(parsed.values(),
                        keys=parsed.keys(),
-                       names=['row','window'],
+                       names=['obs_index','obs_window'],
                        axis=0)
-    program_df.index.name = 'row'
-    df = program_df.merge(parsed, how='inner', on='row').set_index(parsed.index)
+    program_df.index.name = 'obs_index'
+    df = program_df.merge(parsed, how='inner', on='obs_index').set_index(parsed.index)
     df.drop(columns="planWindow", inplace=True)
     return df
 
@@ -66,9 +73,12 @@ def filter_miri(visit_list):
     """Only keep MIRI observations"""
     pop_list = []
     for i, el in enumerate(visit_list):
-        if "MIRI" not in el['configuration'].upper():
+        # if "MIRI" not in el['configuration'].upper():
+        #     pop_list.append(i)
+        if el['configuration'].lower() != 'miri coronagraphic imaging':
             pop_list.append(i)
-    for i in pop_list[::-1]:
+    pop_list = sorted(pop_list)[::-1]
+    for i in pop_list:
         visit_list.pop(i)    
     return visit_list
 
@@ -83,7 +93,6 @@ def prog2df(info, miri_only=True):
     df.rename(columns={"@observation": "observation", "@visit":"visit"}, inplace=True)
     # split the planWindow column into something sortable
     df = add_plan_windows_to_program_df(df)
-
     return df
 
 def get_program_table(list_of_programs, verbose=True):
@@ -92,10 +101,12 @@ def get_program_table(list_of_programs, verbose=True):
     for pid in list_of_programs:
         print(str(pid))
         info = ow.program_info(pid)
-        programs[info['proposal_id']] = prog2df(info['visit'])
+        programs[info['proposal_id']] = prog2df(info)
         if verbose: 
             print(str(pid) + " finished")
-    programs = pd.concat(programs, names=['pid'])
+    # combine the programs and drop the dummy indices
+    programs = pd.concat(programs, names=['pid']).reset_index()
+    programs.drop(columns=['obs_index', 'obs_window'], inplace=True)
     return programs
 
 ### HTML GENERATION ###
@@ -114,17 +125,27 @@ def body_start_template():
     return template.replace("{insert_date_here}", today)
 
 # bottom of the HTML file
-def body_end_template():
+def body_end_template(table_keys=[]):
     with open(template_path / "body_end_template.txt", 'r') as ff:
         template = ff.read()
+
+    # if there's more than one table to sort, pass the keys
+    if table_keys == []:
+        pass
+    else:
+        sort_lines = '\n'.join(f"$('#sortTable_{k}').DataTable();" for k in table_keys)
+        template = template.replace("$('#sortTable').DataTable();", sort_lines)
     return template
 
 
 # start the body, up to the table
-def table_start_template(columns):
+def table_start_template(columns, sort_key=''):
     with open(template_path / "table_start_template.txt", 'r') as ff:
         template = ff.read()
-    column_text = '\n'.join(f'<th class="align-middle">{c}</th>' for c in columns)
+    if sort_key != '':
+        template = template.replace('id="sortTable"', f' id=sortTable_{sort_key}')
+    column_props = 'class="align-middle"'
+    column_text = '\n'.join(f'<th {column_props}>{c}</th>' for c in columns)
     return template.replace("{replace_text_here_with_columns}", column_text)
 
 # bottom of the HTML file
@@ -139,9 +160,10 @@ def generate_table_rows(database):
         text = "<tr>" + text + "</tr>"
         return text
     row_data = ""
-    for i, row in programs.iterrows():
+    for i, row in database.iterrows():
         row_data = row_data + "\n" + df2html_row(row)
     return row_data
+
 
 def write_html(outfile, database):
     """write the visit info to html"""
@@ -150,14 +172,20 @@ def write_html(outfile, database):
 
         ff.write(body_start_template())
 
-        ff.write(table_start_template(database.columns))
+        tables_gb = database.groupby("status")
+        table_keys = sorted(tables_gb.groups.keys())
+        print("Generating the following tables:")
+        print("\t", ", ".join(table_keys))
+        # make a separate table for each kind of visit status
+        for key, group in tables_gb:
+            group_dropna = group.dropna(how='all', axis=1)
+            ff.write(f"<br><br>Status: {key.title()}<br>\n")
+            ff.write(table_start_template(group_dropna.columns, key))
+            row_data = generate_table_rows(group_dropna)
+            ff.write(row_data)
+            ff.write(table_end_template())
 
-        row_data = generate_table_rows(database)
-        ff.write(row_data)
-
-        ff.write(table_end_template())
-
-        ff.write(body_end_template())
+        ff.write(body_end_template(table_keys))
 
 
 if __name__ == "__main__":
@@ -174,4 +202,8 @@ if __name__ == "__main__":
         2153
     ]
     programs = get_program_table(prog_ids)
-    write_html("~/test.html", programs)
+    try:
+        html_path = Path(sys.argv[1])
+    except:
+        html_path = Path("~/test.html")
+    write_html(str(html_path), programs)
